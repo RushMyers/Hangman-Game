7dc4d4a7965db9102641928e35661a78012b41f6
myersRush <myersrush1986@gmail.com>
2017-03-07 18:45:17 -0500

    basic html, css, started javascript

---
+++ main.js
@@ -0,0 +1,27 @@
+$(document).ready(function() {
+      //check jquery
+      // $(".letter").click(function() {
+      //   alert("jquery working!")
+      // });
+      //create word bank
+    var wordsToChooseFrom = ["bologna", "cheese", "sandwich"];
+
+    //randomly select a word from word bank
+    var randNumber = Math.floor(Math.random()*wordsToChooseFrom.length);
+    var ourWord = wordsToChooseFrom[randNumber];
+    //alert(randNumber);
+    //alert(ourWord);
+    //create blanks for our word
+    //and append it to answer
+    $("#startGame").click(function() {
+        var blankSpaces = "";
+        for(var i = 0; i < ourWord.length; i++) {
+        blankSpaces +="_";
+        }
+      alert(blankSpaces)
+      //var blanks = $('blankSpaces').html;
+      $("#answer").append($(blankSpaces));
+    });
+
+
+});
