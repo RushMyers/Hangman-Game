48f98063d52d1c3cacdd7595746da60ba2729201
myersRush <myersrush1986@gmail.com>
2017-03-09 15:34:49 -0500

    game reset complete, win game, lose game, scoring for both, guesses left.

--- main.js
+++ main.js
@@ -1,82 +1,88 @@
 $(document).ready(function() {
 
-      //create word bank:
     var wordsToChooseFrom = ["bologna", "cheese", "sandwich", "pizza", "barbeque"];
-
-    //randomly select a word from word bank:
     var randomNumber = 0;
     var chosenWord = '';
     var ourWord = [];
     var blankSpaces = [];
-    //var indecesOfLetter = [];
     var theLetter = '';
-    //var arrayHolder = [];
     var wrongGuesses = [];
-    //var resetGame = function() {
-    //    blankSpaces = [];
-    //  }
-    //create blanks for our word:
+    var gamesWon = 0;
+    var addWonGame = function () {
+        gamesWon++ ;
+        $("#gamesWonCounter").html(gamesWon);
+        }
+    var gamesLost = 0;
+    var addGameLost = function() {
+        gamesLost++ ;
+        $("#gamesLostCounter").html(gamesLost);
+    }
+    var guessesLeft = 6;
+    var subtractGuess = function() {
+        guessesLeft-- ;
+        $("#guessesLeftCounter").html(guessesLeft);
+    }
+
     var resetGame = function() {
           while(blankSpaces.length > 0) {
-          blankSpaces.pop();
-          $(".answer").text('please play again');
+                blankSpaces = [];
+                ourWord = [];
+                theLetter = '';
+                randomNumber = 0;
+                chosenWord = '';
+                wrongGuesses = [];
+                guessesLeft = 6;
+                $(".letter").removeClass('alreadyGuessed');
           }
+          $(".answer").text(blankSpaces.join(''));
         };
+
+//                  startGame
     $("#startGame").click(function() {
         //resetGame();
         randomNumber = Math.floor(Math.random()*wordsToChooseFrom.length);
         chosenWord = wordsToChooseFrom[randomNumber];
-        console.log(ourWord);
         for(var i =0; i < chosenWord.length; i++) {
         ourWord.push(chosenWord[i]);
-        //blankSpaces.push("_");
         blankSpaces[i] = "_";
         }
         $(".answer").append(blankSpaces);
         alert(chosenWord);
     })
 
-        //when a letter is clicked
+//              when a letter is clicked
     $(".letter").click(function() {
         $(this).addClass("alreadyGuessed");
         theLetter = this.innerHTML;
         for(var i = 0;i < ourWord.length; i++) {
-            if (theLetter === ourWord[i]) {
-                blankSpaces[i] = theLetter;
-                $('.answer').text(blankSpaces.join(''));
-                if (blankSpaces.indexOf("_") === -1) {
-                    alert('Congratulations! You Won!!!');
-                    resetGame();
-                }
-              }
-          }
 
-        //location.reload();
+//                    Correct Guess
+            if  (theLetter === ourWord[i]) {
+                  blankSpaces[i] = theLetter;
+                  $('.answer').text(blankSpaces.join(''));
 
-        if (!(ourWord.indexOf(theLetter) > -1)) {
-              wrongGuesses.push('wrong');
+//                    Win Game
+                  if (blankSpaces.indexOf("_") === -1) {
+                      alert('Congratulations! You Won!!!');
+                      resetGame();
+                      addWonGame();
+                      }
+            }
         }
-          //else {
-          //    alert('sweet!');
-          //}
-        console.log(wrongGuesses);
+//                    Wrong Guess
+            if (!(ourWord.indexOf(theLetter) > -1)) {
+                  wrongGuesses.push('wrong');
+                  console.log(wrongGuesses);
+                  subtractGuess();
 
-        if (wrongGuesses.length === 6) {
-          alert("Sorry, you lost.");
-          resetGame();
-          }
-          //location.reload();
-          //console.log("");
-
-  })
-          //
-//         $('#reset').click(function() {
-//             while(blankSpaces.length > 0) {
-//               blankSpaces.pop();
-// }
-//         })
-
-
+//                     Lose Game
+                  if (wrongGuesses.length === 6) {
+                      alert("Sorry, you lost.");
+                      resetGame();
+                      addGameLost();
+                      }
+            }
+    });
 });
 
 
